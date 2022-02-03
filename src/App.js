74ce4a2767b2bb71ccfb862ae3d5baa1b267c410
sundaramnavabrinit <sundaram.s@navabrindit.com>
2022-02-03 18:53:05 +0530

    home page UI Fixes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
               path="/"
               element={
                 <>
-                  {/* <Home /> */}
+                  <Home />
                   <Footer />
                 </>
               }
