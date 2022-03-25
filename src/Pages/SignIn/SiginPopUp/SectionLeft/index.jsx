12916bc4cc680d85c75f9a852a3c713ae1bd2af7
Madankumar06 <madankumar.p@navabrindit.com>
2022-03-25 07:51:41 +0530

    seller dashboard changes and forgot password alignments

--- src/Pages/SignIn/SiginPopUp/SectionLeft/index.jsx
+++ src/Pages/SignIn/SiginPopUp/SectionLeft/index.jsx
@@ -10,12 +10,13 @@ import {
   Backdrop,
   InputLabel,
 } from "@mui/material";
+import clsx from "clsx";
 import { isEmailValid, isPasswordValid } from "../../../../utilities";
 import { withStyles } from "@mui/styles";
-import { Clear, FemaleSharp, FlashOnOutlined } from "@mui/icons-material";
+import { Clear } from "@mui/icons-material";
 import SectionRight from "../SectionRight";
 import { useStateValue } from "../../../../store/state";
-import forgot from '../../../../Assets/Home/forgotpassword.svg'
+import forgot from "../../../../Assets/Home/forgotpassword.svg";
 
 const TransitionsModal = ({ classes, openPopUp }) => {
   const [{}, dispatch] = useStateValue();
@@ -59,11 +60,11 @@ const TransitionsModal = ({ classes, openPopUp }) => {
     password: "",
   });
 
-  const [forgotpwdemail, setforgotpwdemail] = useState()
+  const [forgotpwdemail, setforgotpwdemail] = useState();
 
   const handleChangeEmail = (event) => {
-    setforgotpwdemail(event.target.value)
-  }
+    setforgotpwdemail(event.target.value);
+  };
 
   const handleClose = () => {
     setOpen(false);
@@ -163,31 +164,101 @@ const TransitionsModal = ({ classes, openPopUp }) => {
   };
 
   //// Forgor Password ///
-  const [passopen, setpassopen] = useState(false)
+  const [passopen, setpassopen] = useState(false);
   const forgotpass = () => {
-    setpassopen(true)
-  }
+    setpassopen(true);
+  };
   return (
     <>
-    {passopen
-    ?
-      <Modal
-        open={passopen}
-        onClose={() => setpassopen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        className={modal}
-      >
-        <div className={forgotpassword}>
-          <div className={forgotpassword__title}>
-            Forgot Password ?
-            <Clear className={forgotpassword__clearbtn} onClick={() => setpassopen(false)} />
+      {passopen ? (
+        <Modal
+          open={passopen}
+          onClose={() => setpassopen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          className={modal}
+        >
+          <div className={forgotpassword}>
+            <div className={forgotpassword__title}>
+              Forgot Password ?
+              <Clear
+                className={forgotpassword__clearbtn}
+                onClick={() => setpassopen(false)}
+              />
+            </div>
+            <div className={forgotpassword__container}>
+              <div className={forgotpassword__content}>
+                <div className={forgotpassword__email}>
+                  <p className={forgotpassword__para}>
+                    Enter your E-mail address to receive a link to reset
+                    Password.
+                  </p>
+                  <div className="inputfield-box">
+                    <TextField
+                      id="email_address"
+                      name="email_address"
+                      label="E-mail Address"
+                      placeholder="E-mail Address"
+                      fullWidth
+                      autoFocus={true}
+                      autoComplete="off"
+                      value={forgotpwdemail}
+                      InputLabelProps={{
+                        shrink: true,
+                        required: true,
+                        classes: {
+                          asterisk: asterisk,
+                        },
+                      }}
+                      onChange={handleChangeEmail}
+                      variant="outlined"
+                    />
+                  </div>
+                  <Box>
+                    <Button
+                      className={clsx(
+                        forgotpassword__submit,
+                        "button-text btn-secondary"
+                      )}
+                      onClick={() => setpassopen(false)}
+                    >
+                      Submit
+                    </Button>
+                  </Box>
+                </div>
+                <img className={forgotpassword__image} src={forgot} alt="" />
+              </div>
+            </div>
           </div>
-          <div className={forgotpassword__container}>
-            <div className={forgotpassword__content}>
-              <div className={forgotpassword__email}>
-                <p className={forgotpassword__para}>Enter your E-mail address to receive a link to reset Password.</p>
-                <TextField
+        </Modal>
+      ) : (
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={modal}
+          open={open}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <div className={section_main}>
+            <header className={header_section}>
+              <p>Welcome !</p>
+              <Clear className={clear_btn} onClick={() => handleClose()} />
+            </header>
+            <div className={sections}>
+              <div className={section_left}>
+                <p className={info_text_lineNote_one}>
+                  <small>*</small> Texub doesn’t sell or ship to consumers.
+                </p>
+                <p className={info_text_lineNote_two}>
+                  To keep connected please login here
+                </p>
+                <div className={info_text}>Sign-In</div>
+                <div className={input_fields}>
+                  <TextField
                     id="email_address"
                     name="email_address"
                     label="E-mail Address"
@@ -195,7 +266,7 @@ const TransitionsModal = ({ classes, openPopUp }) => {
                     fullWidth
                     autoFocus={true}
                     autoComplete="off"
-                    value={forgotpwdemail}
+                    value={signInData?.email_address}
                     InputLabelProps={{
                       shrink: true,
                       required: true,
@@ -203,114 +274,64 @@ const TransitionsModal = ({ classes, openPopUp }) => {
                         asterisk: asterisk,
                       },
                     }}
-                    onChange={handleChangeEmail}
+                    onChange={handleChangeInput}
                     variant="outlined"
                   />
-                <button onClick={() => setpassopen(false)} className={forgotpassword__submit}>Submit</button>
+                  <InputLabel className={validation_error}>
+                    {inputValidation?.email_address}
+                  </InputLabel>
+                  <TextField
+                    id="password"
+                    name="password"
+                    label="Password"
+                    fullWidth
+                    type="password"
+                    autoComplete="new-password"
+                    placeholder="Password"
+                    value={signInData?.password}
+                    InputLabelProps={{
+                      shrink: true,
+                      required: true,
+                      classes: {
+                        asterisk: asterisk,
+                      },
+                    }}
+                    onChange={handleChangeInput}
+                    variant="outlined"
+                  />
+                  <InputLabel className={validation_error}>
+                    {inputValidation?.password}
+                  </InputLabel>
+                  <FormControlLabel
+                    value={signInData?.keep_me_logged_in}
+                    control={<Checkbox color="color_third" />}
+                    label="Keep me signed in."
+                    labelPlacement="end"
+                    className={checkbox_label}
+                    name="keep_me_logged_in"
+                    onClick={(event) => handleChangeInput(event)}
+                  />
+                  <Box className={button_box}>
+                    <Button
+                      onClick={() => handleClickValidation()}
+                      className={button_signin}
+                    >
+                      Sign In
+                    </Button>
+                  </Box>
+                  <p className={forgot_password} onClick={forgotpass}>
+                    Forgot Password?
+                  </p>
+                </div>
               </div>
-              <img className={forgotpassword__image} src={forgot} alt='' />
-            </div>
-          </div>
-        </div>
-      </Modal>
-    :
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={modal}
-        open={open}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <div className={section_main}>
-          <header className={header_section}>
-            <p>Welcome !</p>
-            <Clear className={clear_btn} onClick={() => handleClose()} />
-          </header>
-          <div className={sections}>
-            <div className={section_left}>
-              <p className={info_text_lineNote_one}>
-                <small>*</small> Texub doesn’t sell or ship to consumers.
-              </p>
-              <p className={info_text_lineNote_two}>
-                To keep connected please login here
-              </p>
-              <div className={info_text}>Sign-In</div>
-              <div className={input_fields}>
-                <TextField
-                  id="email_address"
-                  name="email_address"
-                  label="E-mail Address"
-                  placeholder="E-mail Address"
-                  fullWidth
-                  autoFocus={true}
-                  autoComplete="off"
-                  value={signInData?.email_address}
-                  InputLabelProps={{
-                    shrink: true,
-                    required: true,
-                    classes: {
-                      asterisk: asterisk,
-                    },
-                  }}
-                  onChange={handleChangeInput}
-                  variant="outlined"
-                />
-                <InputLabel className={validation_error}>
-                  {inputValidation?.email_address}
-                </InputLabel>
-                <TextField
-                  id="password"
-                  name="password"
-                  label="Password"
-                  fullWidth
-                  type="password"
-                  autoComplete="new-password"
-                  placeholder="Password"
-                  value={signInData?.password}
-                  InputLabelProps={{
-                    shrink: true,
-                    required: true,
-                    classes: {
-                      asterisk: asterisk,
-                    },
-                  }}
-                  onChange={handleChangeInput}
-                  variant="outlined"
-                />
-                <InputLabel className={validation_error}>
-                  {inputValidation?.password}
-                </InputLabel>
-                <FormControlLabel
-                  value={signInData?.keep_me_logged_in}
-                  control={<Checkbox color="color_third" />}
-                  label="Keep me signed in."
-                  labelPlacement="end"
-                  className={checkbox_label}
-                  name="keep_me_logged_in"
-                  onClick={(event) => handleChangeInput(event)}
-                />
-                <Box className={button_box}>
-                  <Button
-                    onClick={() => handleClickValidation()}
-                    className={button_signin}
-                  >
-                    Sign In
-                  </Button>
-                </Box>
-                <p className={forgot_password} onClick={forgotpass}>Forgot Password?</p>
+              <div className={section_right}>
+                <SectionRight />
               </div>
             </div>
-            <div className={section_right}>
-              <SectionRight />
-            </div>
           </div>
-        </div>
-      </Modal>}
-   </>
+        </Modal>
+      )}
+    </>
   );
 };
 export default withStyles(styles)(TransitionsModal);
