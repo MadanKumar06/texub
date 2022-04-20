dfbe3c7dbf57962fd4b1f4ee8c3c2c4a945a2cf7
sundaramnavabrinit <sundaram.s@navabrindit.com>
2022-04-20 15:10:01 +0530

    bulk upload page

---
+++ src/Components/SellerDashboard/Inventory/Buildupload/index.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+
+export default function CsvReader(){
+    const [csvFile, setCsvFile] = useState();
+    const [csvArray, setCsvArray] = useState([]);
+    // [{name: "", age: 0, rank: ""},{name: "", age: 0, rank: ""}]
+
+    const processCSV = (str, delim=',') => {
+        const headers = str.slice(0,str.indexOf('\n')).split(delim);
+        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+
+        const newArray = rows.map( row => {
+            const values = row.split(delim);
+            const eachObject = headers.reduce((obj, header, i) => {
+                obj[header] = values[i];
+                return obj;
+            }, {})
+            return eachObject;
+        })
+
+        setCsvArray(newArray)
+    }
+
+    const submit = () => {
+        const file = csvFile;
+        console.log
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const text = e.target.result;
+            console.log(text);
+            processCSV(text)
+        }
+
+        reader.readAsText(file);
+    }
+
+    return(
+        <form id='csv-form'>
+            <input
+                type='file'
+                accept='.csv'
+                id='csvFile'
+                onChange={(e) => {
+                    setCsvFile(e.target.files[0])
+                }}
+            >
+            </input>
+            <br/>
+            <button
+                onClick={(e) => {
+                    e.preventDefault()
+                    if(csvFile)submit()
+                }}
+            >
+                Submit
+            </button>
+            <br/>
+            <br/>
+            {csvArray.length>0 ? 
+            <>
+                <table>
+                    <thead>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Rank</th>
+                    </thead>
+                    <tbody>
+                        {
+                            csvArray.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.name}</td>
+                                    <td>{item.age}</td>
+                                    <td>{item.rank}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </> : null}
+        </form>
+    );
+
+}
--- src/Pages/SellerDashboard/index.jsx
+++ src/Pages/SellerDashboard/index.jsx
@@ -9,6 +9,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import Dashboard from "../../Components/SellerDashboard/Dashboard";
 import Inventory from "../../Components/SellerDashboard/Inventory";
 import RegisterProduct from "../../Components/SellerDashboard/Inventory/RegisterProduct";
+import Bulkupload from '../../Components/SellerDashboard/Inventory/Buildupload'
 import SuccessPage from "../../Components/SellerDashboard/Inventory/SuccessPage";
 import UpdateProduct from "../../Components/SellerDashboard/Inventory/UpdateProduct";
 import Orders from "../../Components/SellerDashboard/Orders";
@@ -131,6 +132,7 @@ function SellerDashboard() {
           )}
 
           {currenttab === "registerproduct" && <RegisterProduct />}
+          {currenttab === "bulkupload" && <Bulkupload />}
           {currenttab === "registersuccess" && (
             <SuccessPage msg={registersuccess} />
           )}
