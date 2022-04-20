edd47e3fac9367cd558e5030e95cfae909858fe7
sundaramnavabrinit <sundaram.s@navabrindit.com>
2022-04-20 16:24:32 +0530

    pdp radio button fix

--- src/Components/SellerDashboard/Inventory/Buildupload/index.jsx
+++ src/Components/SellerDashboard/Inventory/Buildupload/index.jsx
@@ -23,7 +23,6 @@ export default function CsvReader(){
 
     const submit = () => {
         const file = csvFile;
-        console.log
         const reader = new FileReader();
 
         reader.onload = function(e) {
