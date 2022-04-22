ff1a1169ca0e57c2d4e0451e9d372e5fcb643b22
sundaramnavabrinit <sundaram.s@navabrindit.com>
2022-04-22 16:47:51 +0530

    endpoint update

--- src/Constant.js
+++ src/Constant.js
@@ -1,7 +1,30 @@
+// module.exports = {
+//   baseUrl: function () {
+//     return "https://uat.texub.com/rest/V1/texub";
+//   },
+//   imageBaseUrl: function () {
+//     return "https://texub.uat.a2zportals.co.in/media/";
+//   },
+//   adminTokenUrl: function () {
+//     return "https://uat.texub.com/rest/V1/integration/admin/token";
+//   },
+//   customerTokenUrl: function () {
+//     return "https://uat.texub.com/rest/V1/integration/customer/token";
+//   },
+//   customerMeDetailUrl: function () {
+//     return "https://uat.texub.com/rest/V1/customers/me";
+//   },
+//   baseUrl2: function () {
+//     return "https://uat.texub.com/rest/V1";
+//   },
+//   pdfDowloadUrl: function () {
+//     return "https://uat.texub.com";
+//   },
+// };
+
 module.exports = {
   baseUrl: function () {
     return "https://texub.uat.a2zportals.co.in/rest/V1/texub";
-    //  return "https://uat.texub.com/rest/V1/texub";
   },
   imageBaseUrl: function () {
     return "https://texub.uat.a2zportals.co.in/media/";
