073664b0aebb7d4009f6d562622e1a4c2c91d16b
Vijaykumar S <vijaykumar@ambab.com>
2022-04-22 11:31:23 +0530

    polaiah shared code

--- src/Pages/MyCart/MyCartTable/index.jsx
+++ src/Pages/MyCart/MyCartTable/index.jsx
@@ -277,7 +277,7 @@ const MyCartTable = ({ cartDataList, deleteCartData }) => {
     },
     {
       name: "price",
-      label: "PRICE",
+      label: "PRICE / UNIT",
       options: {
         customBodyRender: (value) => {
           return (
@@ -285,7 +285,7 @@ const MyCartTable = ({ cartDataList, deleteCartData }) => {
               <div className="mycart_table_price">
                 <span>INR</span>
                 {value}
-                <span>/unit</span>
+                {/* <span>/unit</span> */}
               </div>
             </div>
           );
