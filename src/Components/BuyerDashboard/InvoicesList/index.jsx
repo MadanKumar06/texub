9e95bcb2aa60ae9d141ad833c53fba302f62f50e
Madankumar06 <madankumar.p@navabrindit.com>
2022-04-28 19:13:07 +0530

    pending invoice

--- src/Components/BuyerDashboard/InvoicesList/index.jsx
+++ src/Components/BuyerDashboard/InvoicesList/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import MUITable from '../../../Components/Common/MUITable'
+import MUITable from "../../../Components/Common/MUITable";
 import { ArrowBackIosNew } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import Pagination from "../../Pagination";
 import "./styles.scss";
-import Vieworders from '../../Common/Vieworders'
-import transaction_type from "../../../Assets/buyerdashboard/InvoicesList/paypal (1).png"
-import transaction_type1 from "../../../Assets/buyerdashboard/InvoicesList/braintree-logo-black.png"
+import Vieworders from "../../Common/Vieworders";
+import transaction_type from "../../../Assets/buyerdashboard/InvoicesList/paypal (1).png";
+import transaction_type1 from "../../../Assets/buyerdashboard/InvoicesList/braintree-logo-black.png";
 import { useStateValue } from "../../../store/state";
 function Index() {
   const [tableData, setTableData] = useState([]);
@@ -15,27 +15,27 @@ function Index() {
     { name: "Pending Invoices" },
     { name: "Paid Invoices" },
   ];
-  const [{geo, customstore, customnostore}, dispatch] = useStateValue()
+  const [{ geo, customstore, customnostore }, dispatch] = useStateValue();
   const [type, settype] = useState();
 
   const selectorder = (value) => {
     settype(value);
   };
-  
+
   useEffect(() => {
-    selectorder(0)
-  }, [])
+    selectorder(0);
+  }, []);
 
   const PaginateDataSplit = (event) => {
     setTableData(event);
   };
 
-  const [isVieworders, setisVieworders] = useState(false)
+  const [isVieworders, setisVieworders] = useState(false);
   const orders = () => {
-    setisVieworders(true)
-    setisOrders(false)
-  }
-  const [isOrders, setisOrders] = useState(true)
+    setisVieworders(true);
+    setisOrders(false);
+  };
+  const [isOrders, setisOrders] = useState(true);
 
   const options = {
     filter: false,
@@ -55,41 +55,41 @@ function Index() {
       date: "08/05/21",
       sellerid: "INDS2013",
       hub: "Mumbai",
-      amount:"30,427,563",
+      amount: "30,427,563",
       transactionmode: transaction_type,
       status: "Pending",
       action: "View Details",
-    },    
+    },
     {
-        invoiceno: "00000034",
-        date: "28/09/21",
-        sellerid: "INDS2019",
-        hub: "New Delhi",
-        amount:"42,427,563",
-        transactionmode: transaction_type1,
-        status: "Pending",
-        action: "View Details",
-      },
-      {
-        invoiceno: "00000045",
-        date: "26/02/21",
-        sellerid: "SINS20537",
-        hub: "Singapore",
-        amount:"23,427,563",
-        transactionmode: transaction_type,
-        status: "Paid",
-        action: "View Details",
-      },
-      {
-        invoiceno: "00000065",
-        date: "26/04/21",
-        sellerid: "UAES2517",
-        hub: "Dubai",
-        amount:"3,427,563",
-        transactionmode: transaction_type1,
-        status: "Paid",
-        action: "View Details",
-      },
+      invoiceno: "00000034",
+      date: "28/09/21",
+      sellerid: "INDS2019",
+      hub: "New Delhi",
+      amount: "42,427,563",
+      transactionmode: transaction_type1,
+      status: "Pending",
+      action: "View Details",
+    },
+    {
+      invoiceno: "00000045",
+      date: "26/02/21",
+      sellerid: "SINS20537",
+      hub: "Singapore",
+      amount: "23,427,563",
+      transactionmode: transaction_type,
+      status: "Paid",
+      action: "View Details",
+    },
+    {
+      invoiceno: "00000065",
+      date: "26/04/21",
+      sellerid: "UAES2517",
+      hub: "Dubai",
+      amount: "3,427,563",
+      transactionmode: transaction_type1,
+      status: "Paid",
+      action: "View Details",
+    },
   ];
 
   const columns = [
@@ -113,24 +113,28 @@ function Index() {
       },
     },
     {
-        name: "amount",
-        label: "Amount",
-        options: {
-          customBodyRender: (value) => {
-            return <div className="invoices__amount">
-                  <span className="currency">INR </span>
-                  <span className="price">{value}</span>
-            </div>;
-          },
+      name: "amount",
+      label: "Amount",
+      options: {
+        customBodyRender: (value) => {
+          return (
+            <div className="invoices__amount">
+              <span className="currency">INR </span>
+              <span className="price">{value}</span>
+            </div>
+          );
         },
       },
-    { name: "hub", label: "HUB",
-    options: {
+    },
+    {
+      name: "hub",
+      label: "HUB",
+      options: {
         customBodyRender: (value) => {
           return <div className="invoices__hub">{value}</div>;
         },
       },
-     },
+    },
     {
       name: "transactionmode",
       label: "Transaction Mode",
@@ -167,24 +171,19 @@ function Index() {
       label: "Action",
       options: {
         customBodyRender: (value) => {
-          return <div className="invoices__action" onClick={orders}>{value}</div>;
+          return (
+            <div className="invoices__action" onClick={orders}>
+              {value}
+            </div>
+          );
         },
       },
     },
   ];
 
   return (
-
     <div className="invoices_main">
-      <div className="invoices__footer">
-        <div className="invoices__container">
-          <Link to={`/${customnostore ? customnostore : geo?.country_name}/buyerdashboard/dashboard`}>
-            <ArrowBackIosNew />
-            <span>Back</span>
-          </Link>
-        </div>
-      </div>
-      {isOrders &&
+      {isOrders && (
         <>
           <div className="invoices__buttons">
             {ordertype.map((data, i) => (
@@ -197,16 +196,37 @@ function Index() {
               </p>
             ))}
           </div>
-          <MUITable columns={columns} table={tableData} options={options} className="invoices__table" />
+          <MUITable
+            columns={columns}
+            table={tableData}
+            options={options}
+            className="invoices__table"
+          />
           <Pagination
             PaginateData={PaginateDataSplit}
             DataList={table}
             PagePerRow={10}
           />
+          <div className="invoices__footer">
+            <div className="invoices__container">
+              <Link
+                to={`/${
+                  customnostore ? customnostore : geo?.country_name
+                }/buyerdashboard/dashboard`}
+              >
+                <ArrowBackIosNew />
+                <span>Back</span>
+              </Link>
+            </div>
+          </div>
         </>
-
-      }
-      {isVieworders && <Vieworders />}
+      )}
+      {isVieworders && (
+        <Vieworders
+          setisVieworders={setisVieworders}
+          setisOrders={setisOrders}
+        />
+      )}
     </div>
   );
 }
