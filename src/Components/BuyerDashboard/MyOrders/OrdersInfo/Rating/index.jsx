53e9ca5a76d0f442aebc30b4282426ea67aa869c
Madankumar06 <madankumar.p@navabrindit.com>
2022-05-15 22:12:08 +0530

    smart recommentdation search

--- src/Components/BuyerDashboard/MyOrders/OrdersInfo/Rating/index.jsx
+++ src/Components/BuyerDashboard/MyOrders/OrdersInfo/Rating/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Modal, Backdrop, InputLabel, Rating, Button } from "@mui/material";
-import TextareaAutosize from "@mui/base/TextareaAutosize";
 import { TextField } from "@mui/material";
 
 import { Clear } from "@mui/icons-material";
@@ -13,7 +12,7 @@ import swal from "sweetalert2";
 export default function BasicModal({ Popup, currentorder }) {
   // const [{}, dispatch] = useStateValue();
 
-  const [{ geo, customnostore }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
   const [rating, setrating] = useState({
     star: 0,
     comment: "",
@@ -118,34 +117,39 @@ export default function BasicModal({ Popup, currentorder }) {
             <div className="rating_comments_block">
               <InputLabel>Comments</InputLabel>
 
-               <TextField
-                  className="inputfield-box contact-form-inputfieldbox"
-                  fullWidth
-                  aria-label="comments"
-                  placeholder="Type your message"
-                  name="your_message"
-                  id="your_message"
-                  multiline
-                  minRows={3}
-                  style={{ height: 100 }}
-                    value={rating?.comment}
-                    onChange={(e) =>
-                      setrating((rating) => ({
-                        ...rating,
-                        comment: e.target.value,
-                      }))
-                    }
-                  InputLabelProps={{
-                    shrink: true,
-                    required: true,
-                    classes: {
-                      asterisk: "asterisk",
-                    },
-                  }}
-                  variant="outlined"
-               />
+              <TextField
+                className="inputfield-box contact-form-inputfieldbox"
+                fullWidth
+                aria-label="comments"
+                placeholder="Type your message"
+                name="your_message"
+                id="your_message"
+                multiline
+                rows={3}
+                style={{ height: 100 }}
+                value={rating?.comment}
+                onChange={(e) =>
+                  setrating((rating) => ({
+                    ...rating,
+                    comment: e.target.value,
+                  }))
+                }
+                InputLabelProps={{
+                  shrink: true,
+                  required: true,
+                  classes: {
+                    asterisk: "asterisk",
+                  },
+                }}
+                variant="outlined"
+              />
               <div className="rating-btns">
-                <Button className="rating_comments_cancel" onClick={()=>handleClose()}>Cancel</Button>
+                <Button
+                  className="rating_comments_cancel"
+                  onClick={() => handleClose()}
+                >
+                  Cancel
+                </Button>
                 <Button
                   className="btn-secondary rating_comments_submit"
                   onClick={reviewsubmit}
--- src/Pages/SellerDashboard/index.jsx
+++ src/Pages/SellerDashboard/index.jsx
@@ -98,18 +98,18 @@ function SellerDashboard() {
   const sidebarstate = () => {
     setbarstate(true);
   };
-  const [searchupdate, setsearchupdate] = useState(false)
+  const [searchupdate, setsearchupdate] = useState(false);
   const updatesearch = (e) => {
-    e.preventDefault()
-    setsearchupdate(!searchupdate)
-  }
+    e.preventDefault();
+    setsearchupdate(!searchupdate);
+  };
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    setSearch("")
-  }, [currenttab])
-  console.log(search)
+    setSearch("");
+  }, [currenttab]);
+
   return (
     <div className="sellerdashboard">
       {/* <img src={bg} alt="" /> */}
@@ -142,7 +142,8 @@ function SellerDashboard() {
                   inputProps={{ "aria-label": "search google maps" }}
                   className="sellerdashboard__input"
                   onChange={(e) => setSearch(e.target.value)}
-                  onKeyPress={(e) => e.key === 'Enter' && updatesearch(e)}
+                  value={search}
+                  onKeyPress={(e) => e.key === "Enter" && updatesearch(e)}
                 />
                 <IconButton
                   type="submit"
@@ -186,7 +187,9 @@ function SellerDashboard() {
           )}
           {currenttab === "addsuccess" && <SuccessPage msg={addsuccess} />}
 
-          {currenttab === "orders" && <Orders searchdata={search} searchupdate={searchupdate}/>}
+          {currenttab === "orders" && (
+            <Orders searchdata={search} searchupdate={searchupdate} />
+          )}
 
           {currenttab === "usermgmt" && (
             <MyProfile />
@@ -195,9 +198,16 @@ function SellerDashboard() {
 
           {currenttab === "sellerservices" && <Sellerservices />}
 
-          {currenttab === "directenquiries" && <Directenqueries searchdata={search} searchupdate={searchupdate}/>}
+          {currenttab === "directenquiries" && (
+            <Directenqueries searchdata={search} searchupdate={searchupdate} />
+          )}
           {currenttab === "paymentmethods" && <Paymentmethods />}
-          {currenttab === "smart-recommendation" && <SmartRecommendation searchdata={search} searchupdate={searchupdate} />}
+          {currenttab === "smart-recommendation" && (
+            <SmartRecommendation
+              searchdata={search}
+              searchupdate={searchupdate}
+            />
+          )}
         </div>
       </div>
     </div>
