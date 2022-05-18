1d0931126da65b23e907deb072a30efa7b80f773
Vijaykumar S <vijaykumar@ambab.com>
2022-05-18 19:32:47 +0530

    asana bug tickets

--- src/Components/Home/SubscriptionInfo/SubscriptionInfo.js
+++ src/Components/Home/SubscriptionInfo/SubscriptionInfo.js
@@ -5,11 +5,13 @@ import "./styles.scss";
 export const SubscriptionInfo = ({ data }) => {
     return (
         <div className="subscription_required_block">
+            <div className="subscript_image">
             <div >
             <img className="Benfits_img" src={data?.banneimage1} alt="" />
             </div>
             <div >
                 <img className="Benfits_img" src={data?.banneimage2} alt="" />
+            </div>
             </div>
             <div className="subscription_content_section">
                 <h2 className="subscription_title">{data?.title}</h2>
